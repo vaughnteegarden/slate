e635cd78f48b14a2f11a6ac89b831eb1912ddc2d
Vaughn Teegarden <meesterx@gmail.com>
2018-05-23 12:55:03 -0400

    index for beta site

---
+++ index.php for beta site/index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <p>Please visit the live SDK docs location: <a href="http://rezolve.com/docs/">http://rezolve.com/docs/</a>. 
+
+ 
+ <p>Beta docs, if any, are here:  <a href="http://rezolve.com/docsdev/index.html">http://rezolve.com/docsdev/index.html</a>.  
+
+ 
+ </body>
+</html>
